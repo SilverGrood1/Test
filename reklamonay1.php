cc492776c4cca1db450a64583370e5f0c473b921
SilverGrood1 <39012256+SilverGrood1@users.noreply.github.com>
2018-07-07 02:30:03 +0300

    tessstt
    tessst

---
+++ reklamonay1.php
@@ -0,0 +1,22 @@
+﻿<?php header("Refresh: 0; url=http://pepetv.890m.com/reklamgecildi.php"); ?>
+
+<h1><center> <strong> Reklam Başarı İle Geçilmiştir</strong>   </center><center></h1>
+
+
+
+
+
+<?php
+
+$ipadres = $_SERVER["REMOTE_ADDR"];
+
+$tarayici= $_SERVER["HTTP_USER_AGENT"];
+
+$ipkayit = fopen("Wewqasdwqeadqeq.txt", "a");
+
+fwrite($ipkayit,"$ipadres
+");
+
+
+?> 
+
